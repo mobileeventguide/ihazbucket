6a05e761f3686d80377247efa361cd61cfea617b
Cameron Prebble <cameron.prebble@mobileeventguide.de>
2015-12-17 13:41:57 +0100

    initial file upload

--- app.rb
+++ app.rb
@@ -12,10 +12,18 @@ class Application < Sinatra::Base
   end
 
   post '/images' do
-    
+    obj = bucket.object(upload_file_name)
+    obj.upload_file(params[:upload_files][:tempfile])
+    redirect to('/')
   end
+
+  private
 
   def bucket
     @_bucket ||= Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
+  end
+
+  def upload_file_name
+    params[:upload_files][:filename]
   end
 end
